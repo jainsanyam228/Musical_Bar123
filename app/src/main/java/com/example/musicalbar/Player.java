8b11d57bc50722bce23d71ac6a02a2d6356848d6
SANYAM <sanyamjain22800@gmail.com>
2021-06-22 14:10:51 +0530

    still working

---
+++ app/src/main/java/com/example/musicalbar/Player.java
@@ -0,0 +1,14 @@
+package com.example.musicalbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
